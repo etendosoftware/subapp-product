21111e0e2ddc32bd42007fb5b48bd4c75762f7aa
sebastianbarrozo <sebastian.barrozo@etendo.software>
2023-12-20 10:06:22 -0300

    Feature EPL-1164: React generated code migrated

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
 module.exports = {
   preset: 'react-native',
+  setupFiles: ['<rootDir>/jest.setup.js'],
 };
--- src/hooks/useProduct.ts
+++ src/hooks/useProduct.ts
@@ -22,17 +22,8 @@ export const useProduct = () => {
 
   const createProduct = async (body: Product) => {
     const defaultValues: Product = {
-      active: true,
-      description: 'default',
       name: body.name,
-      organization: '/B843C30461EA4501935CB1D125C9C25A',
-      productCategory: '/DC7F246D248B4C54BFC5744D5C27704F',
-      productType: 'I',
-      productValue: 'default',
-      searchKey: `ES/00${generateRandomNumber()}`,
       uPCEAN: body.uPCEAN,
-      taxCategory: '/E020A69A1E784DC39BE57C41D6D5DB4E',
-      uOM: '/100',
     };
     const res = await ProductService.BACK.save(defaultValues);
     return res;
